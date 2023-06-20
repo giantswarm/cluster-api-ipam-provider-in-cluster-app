315f0f49c164bd5782a40a3c20d5b9d4b0ad987c
Jirka Kremser <jiri.kremser@gmail.com>
2023-06-20 15:19:14 +0200

    sync manifest from upstream
    Signed-off-by: Jirka Kremser <jiri.kremser@gmail.com>

---
+++ Makefile.custom.mk
@@ -0,0 +1,30 @@
+##@ App
+
+SHELL := /bin/bash
+
+APPLICATION_NAME="cluster-api-ipam-provider-in-cluster"
+
+UPSTREAM_ORG="kubernetes-sigs"
+REPO_NAME="cluster-api-ipam-provider-in-cluster"
+TAG_TO_SYNC="v0.1.0-alpha.2"
+
+.PHONY: all
+all: fetch-upstream-manifest apply-kustomize-patches delete-generated-helm-charts release-manifests ## Builds the manifests to publish with a release (alias to release-manifests)
+
+.PHONY: fetch-upstream-manifest
+fetch-upstream-manifest: ## fetch upstream manifest from upstream repo
+	./hack/sync-version.sh ${UPSTREAM_ORG} ${REPO_NAME} ${TAG_TO_SYNC}
+
+.PHONY: apply-kustomize-patches
+apply-kustomize-patches: ## apply giantswarm specific patches
+	kubectl kustomize config/kustomize -o config/kustomize/tmp
+
+.PHONY: delete-generated-helm-charts
+delete-generated-helm-charts: # clean workspace and delete manifests
+	@rm -rvf ./helm/${APPLICATION_NAME}/templates/*.yaml
+	@rm -rvf ./helm/${APPLICATION_NAME}/files/*.yaml
+
+.PHONY: release-manifests
+release-manifests:
+	# move files from workdir over to helm directury structure
+	./hack/prepare-helmchart.sh ${APPLICATION_NAME}
